9b933cb403390057c5eaadc9f8cff077879dbed5
Divyanshu Tiwari <divyanshu.tiwari33@gmail.com>
2020-08-31 21:29:55 +0530

    Add logic to the calculator

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,27 +3,99 @@ import Screen from './Screen';
 import Numpad from './Numpad';
 import Operatorpad from './Operatorpad';
 import Functionpad from './Functionpad';
+import {category} from './category';
 
 class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: '',
-    }
+      value1: '',
+      value2: '',
+      display: '',
+      operator: '',
+      result: '',
+    };
     this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick(value) {
-    if(value == 'AC') {
-      this.setState({
-        value: '',
-      })
+  computeResult() {
+    const operator = this.state.operator;
+    const value1 = parseFloat(this.state.value1);
+    const value2 = parseFloat(this.state.value2);
+
+    let result;
+    switch(operator) {
+      case '+':
+        result = value1 + value2; 
+        break;
+      case '-':
+        result = value1 - value2;
+        break;
+      case '×':
+        result = value1 * value2;
+        break;
+      case '÷':
+        result = value1 / value2;
+        break;
+      default:
+        break; 
     }
-    else {
+    console.log(result);
+    return result;
+  }
+
+  handleClick(value, type) {
+    if(type === category.CLEAR) {
+      this.setState({
+        value1: '',
+        value2: '',
+        display: '',
+        operator: '',
+        result: '',
+      });
+    }
+    else if(type === category.NUMBER) {
+      if(this.state.operator === '') {
+        this.setState(
+          (state) => {
+            const v = state.value1 + value.toString();
+            return {
+              value1: v,
+              display: v,
+            };
+          }
+        );
+      }
+      else {
+        this.setState(
+          (state) => {
+            const v = state.value2 + value.toString();
+            return {
+              value2: v,
+              display: v,
+            };
+          }
+        );
+      }
+    }
+    else if(type === category.OPERATOR) {
+      this.setState({
+        operator: value,
+        display: '',
+      });
+    }
+    else if(type === category.COMPUTE) {
       this.setState(
-        (state) => ({
-          value: state.value + value.toString(),
-        })
+        (state) => {
+          const r = this.computeResult();
+          return {
+            value1: r,
+            value2: '',
+            display: r,
+            operator: '',
+            result: r,
+          };
+        }
       );
     }
   }
@@ -31,7 +103,7 @@ class Board extends React.Component {
   render() {
     return (
       <div style={boardStyle}>
-        <Screen value={this.state.value} />
+        <Screen value={this.state.display} />
         <div style={compositionStyle1}>
           <div style={compositionStyle2}>
             <Numpad handleClick={this.handleClick} />
