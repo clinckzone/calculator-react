056766e7e56b7229ae917c6198f9bde5c7f7aec1
Divyanshu Tiwari <divyanshu.tiwari33@gmail.com>
2020-08-31 15:28:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import Board from './components/Board';
 
-function App() {
-  return (
-    <div style={appStyle}>
-      <h1>Calculator</h1>
-      <Board/>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div style={appStyle}>
+        <h1>Calculator</h1>
+        <Board/>
+      </div>
+    );
+  }
 }
 
 const appStyle = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
   fontFamily: 'Arial',
   fontSize: 20,
 }
