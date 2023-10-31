ea46605f79dc480f8af6a4be6d3ad7b8118237b0
clinckzone <divyanshu.tiwari33@gmail.com>
2023-10-31 21:59:20 +0530

    Updates

---
+++ precache-manifest.d78d71aac7500cd41ca1779e1d519b30.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4767a20c779d590e1bced33a17ca9383",
+    "url": "/calculator-react/index.html"
+  },
+  {
+    "revision": "7061e8378d177bb6d74c",
+    "url": "/calculator-react/static/js/2.43608a0b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/calculator-react/static/js/2.43608a0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c93fa9255cd6bb9d8c9c",
+    "url": "/calculator-react/static/js/main.b6dc4493.chunk.js"
+  },
+  {
+    "revision": "079e5d957a18fa198292",
+    "url": "/calculator-react/static/js/runtime-main.2efe6b2e.js"
+  }
+]);
