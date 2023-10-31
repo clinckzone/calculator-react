1d99e2ce6449a3d2e9adfb9c6af805ac447188cd
clinckzone <divyanshu.tiwari33@gmail.com>
2023-10-31 21:56:01 +0530

    Fix indentation issue and rename file

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -6,63 +6,63 @@ import Functionpad from './Functionpad';
 import { computeResult } from '../expression-interpreter';
 
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      display: '',
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			display: '',
+		};
+		this.handleClick = this.handleClick.bind(this);
+	}
 
-  handleClick(value) {
-    if (value !== '=') {
-      if (value === 'AC') {
-        this.setState({
-          display: '',
-        });
-      } else {
-        this.setState((state) => {
-          const d = state.display + value.toString();
-          return {
-            display: d,
-          };
-        });
-      }
-    } else {
-      this.setState({
-        display: computeResult(this.state.display),
-      });
-    }
-  }
+	handleClick(value) {
+		if (value !== '=') {
+			if (value === 'AC') {
+				this.setState({
+					display: '',
+				});
+			} else {
+				this.setState((state) => {
+					const d = state.display + value.toString();
+					return {
+						display: d,
+					};
+				});
+			}
+		} else {
+			this.setState({
+				display: computeResult(this.state.display),
+			});
+		}
+	}
 
-  render() {
-    return (
-      <div style={boardStyle}>
-        <Screen value={this.state.display} />
-        <div style={compositionStyle1}>
-          <div style={compositionStyle2}>
-            <Numpad handleClick={this.handleClick} />
-            <Functionpad handleClick={this.handleClick} />
-          </div>
-          <Operatorpad handleClick={this.handleClick} />
-        </div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div style={boardStyle}>
+				<Screen value={this.state.display} />
+				<div style={compositionStyle1}>
+					<div style={compositionStyle2}>
+						<Numpad handleClick={this.handleClick} />
+						<Functionpad handleClick={this.handleClick} />
+					</div>
+					<Operatorpad handleClick={this.handleClick} />
+				</div>
+			</div>
+		);
+	}
 }
 
 const compositionStyle1 = {
-  display: 'flex',
-  flexDirection: 'row',
+	display: 'flex',
+	flexDirection: 'row',
 };
 
 const compositionStyle2 = {
-  display: 'flex',
-  flexDirection: 'column',
+	display: 'flex',
+	flexDirection: 'column',
 };
 
 const boardStyle = {
-  width: 200,
+	width: 200,
 };
 
 export default Board;
