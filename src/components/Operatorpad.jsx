95c333cf58fd54adee471e70f29d38fa4e9cc1ea
clinckzone <divyanshu.tiwari33@gmail.com>
2023-11-01 11:18:55 +0530

    Fix indentation issues

--- src/components/Operatorpad.jsx
+++ src/components/Operatorpad.jsx
@@ -2,28 +2,28 @@ import React from 'react';
 import Button from './Button';
 
 class Operatorpad extends React.Component {
-	generateLayout() {
-		const layout = [
-			<div>
-				<Button handleClick={() => this.props.handleClick('+')} value='+' />
-			</div>,
-			<div>
-				<Button handleClick={() => this.props.handleClick('-')} value='-' />
-			</div>,
-			<div>
-				<Button handleClick={() => this.props.handleClick('×')} value='×' />
-			</div>,
-			<div>
-				<Button handleClick={() => this.props.handleClick('÷')} value='÷' />
-			</div>,
-		];
-		return layout;
-	}
+  generateLayout() {
+    const layout = [
+      <div>
+        <Button handleClick={() => this.props.handleClick('+')} value='+' />
+      </div>,
+      <div>
+        <Button handleClick={() => this.props.handleClick('-')} value='-' />
+      </div>,
+      <div>
+        <Button handleClick={() => this.props.handleClick('×')} value='×' />
+      </div>,
+      <div>
+        <Button handleClick={() => this.props.handleClick('÷')} value='÷' />
+      </div>,
+    ];
+    return layout;
+  }
 
-	render() {
-		const layout = this.generateLayout();
-		return <div>{layout}</div>;
-	}
+  render() {
+    const layout = this.generateLayout();
+    return <div>{layout}</div>;
+  }
 }
 
 export default Operatorpad;
